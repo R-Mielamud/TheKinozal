46325d0eb10b5cac7c98f3bc1813e5dfac8f9a4f
R-Mielamud <2m.roman2@gmail.com>
2020-10-11 00:25:24 +0300

    Added video name field

--- videos/models.py
+++ videos/models.py
@@ -2,7 +2,8 @@ from django.db.models import *
 
 
 class Video(Model):
+    name = CharField(max_length=100)
     youtube_id = CharField(max_length=100)
 
     def __str__(self):
-        return self.youtube_id
+        return self.name
