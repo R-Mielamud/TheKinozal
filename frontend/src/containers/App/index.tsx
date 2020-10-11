42c8581c0162d3ab80c884e45edf853a64665076
R-Mielamud <2m.roman2@gmail.com>
2020-10-11 14:52:36 +0300

    Initialized react app

---
+++ frontend/src/containers/App/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Header } from "semantic-ui-react";
+
+const App: React.FC = () => {
+    return (
+        <Header>Hello, world!</Header>
+    );
+};
+
+export default App;
