275670b8a4d6a62d9e9ab4c0dbc28fd150fc2f1c
R-Mielamud <2m.roman2@gmail.com>
2020-10-14 20:22:04 +0300

    Prepare for deploy

--- TheKinozal/settings.py
+++ TheKinozal/settings.py
@@ -90,12 +90,24 @@ WSGI_APPLICATION = 'TheKinozal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'thekinozal',
+            'USER': 'postgres',
+            'PASSWORD': '*postgres#admin*',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
