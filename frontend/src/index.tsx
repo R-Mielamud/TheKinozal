5f0d569f052142c4cee0c421e57b0b746e46262f
R-Mielamud <2m.roman2@gmail.com>
2020-10-17 21:32:43 +0300

    Fix

---
+++ frontend/src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import * as serviceWorker from './serviceWorker';
+import automaticConfigureLanguages from './config/i18next.config';
+
+import 'react-notifications/lib/notifications.css';
+import 'semantic-ui-css/semantic.min.css';
+import './styles/index.scss';
+
+automaticConfigureLanguages();
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<App />, rootElement);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
