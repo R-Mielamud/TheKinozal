3eec48fa69fe958186440a5e2476ec79b5853353
R-Mielamud <2m.roman2@gmail.com>
2020-12-09 23:22:54 +0200

    Redeploy

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -17,3 +17,4 @@ ReactDOM.render(<App />, rootElement);
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+// test
